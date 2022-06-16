0a29ba90b4fa9b3abbb5971cd700fd77749441c1
Ngoakor12 <ngoakor12@gmail.com>
2022-06-16 10:53:01 +0200

    chore: move frontend code to frontend folder

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterSlice from "../features/counter/counter-slice";
-
-export const store = configureStore({
-  reducer: {
-    counter: counterSlice,
-  },
-});

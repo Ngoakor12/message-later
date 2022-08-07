a64a665200cc5fb57681291cd6631bb93a27ea8c
Ngoakor12 <ngoakor12@gmail.com>
2022-08-07 18:57:43 +0200

    chore: unfinished message details and summary components

---
+++ frontend/src/features/messages/MessageSummary.jsx
@@ -0,0 +1,26 @@
+function MessageSummary() {
+  return (
+    <div>
+      <div>
+        <div>status icon</div>
+      </div>
+      <div>
+        <div>title</div>
+        <div>
+          <div>time</div>
+          <div>recipient name</div>
+        </div>
+      </div>
+      <div>
+        <div>options button</div>
+        <div>
+          options
+          <div>edit</div>
+          <div>delete</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageSummary;

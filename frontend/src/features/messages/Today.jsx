b8543ae8e650165549f0401ee34ecf75cddd7fc3
Ngoakor12 <ngoakor12@gmail.com>
2022-08-08 14:10:43 +0200

    chore: add mock data

--- frontend/src/features/messages/Today.jsx
+++ frontend/src/features/messages/Today.jsx
@@ -1,9 +1,14 @@
 import ScheduleMessageButton from "../../components/ScheduleMessageButton";
+import messages from "../../mock-messages";
+import MessageSummary from "./MessageSummary";
 
 function Today() {
   return (
     <>
       <h2>Today</h2>
+      {messages.map((message) => {
+        return <MessageSummary message={message} />;
+      })}
       <ScheduleMessageButton />
     </>
   );
---
+++ frontend/src/mock-messages.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "I just got Disney+",
+    body: "I was just testing out Disney plus since it landed today in SA. Just wanted to say you can also test it out using my number, who know maybe I'll keep paying if it's good.",
+    from: "Ngoako",
+    time: "12:45",
+    day: "8 August 2022",
+    recipientName: "Valentine",
+  },
+  {
+    title: "Happy Birthday!",
+    body: "Happy birthday bro, hope it's a good one filled with food and laughter.",
+    from: "Ngoako",
+    time: "12:45",
+    day: "8 August 2022",
+    recipientName: "Valentine",
+  },
+];

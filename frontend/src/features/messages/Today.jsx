009b1d4681a621df4f711e13c8bf0d08953eaeae
Ngoakor12 <ngoakor12@gmail.com>
2022-08-20 07:45:10 +0200

    chore: add date on the today page

--- frontend/src/features/messages/Today.jsx
+++ frontend/src/features/messages/Today.jsx
@@ -3,9 +3,13 @@ import MessageList from "./MessageList";
 
 function Today() {
   return (
-    <>
+    <main className="today-main">
+      <div className="today-date-container">
+        <div className="horizontal-line"></div>
+        <div className="today-date">20 September 2022</div>
+      </div>
       <MessageList messages={messages} />
-    </>
+    </main>
   );
 }
 

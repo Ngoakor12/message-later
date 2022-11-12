e2df1a1dcb886724dbd30bf98fc0cf31cdc5fdbf
ngoakor12 <ngoakor12@gmail.com>
2022-11-12 21:03:43 +0200

    chore: create message function

--- backend/db/functions.js
+++ backend/db/functions.js
@@ -3,6 +3,7 @@ const { pool } = require("./config");
 const {
   createMessageTableQuery,
   deleteMessagesTableQuery,
+  createMessageQuery,
 } = require("./queries");
 
 async function createMessagesTable() {
@@ -13,6 +14,39 @@ async function deleteTable() {
   return await pool.query(deleteMessagesTableQuery);
 }
 
+async function createMessage(
+  name,
+  method,
+  contact,
+  title,
+  body,
+  from,
+  day,
+  time
+) {
+  return await pool.query(createMessageQuery, [
+    name,
+    method,
+    contact,
+    title,
+    body,
+    from,
+    day,
+    time,
+  ]);
+}
+
 // createMessagesTable().then((res) => console.log(res));
 
 // deleteTable().then((res) => console.log(res));
+
+// createMessage(
+//   "halaand",
+//   "email",
+//   "halaand@mancity.com",
+//   "question",
+//   "are you a robot?",
+//   "not police",
+//   "12-12-2022",
+//   "12:00"
+// ).then((res) => console.log(res));
--- backend/db/queries.js
+++ backend/db/queries.js
@@ -10,5 +10,8 @@ module.exports = {
         day varchar,
         time varchar
     );`,
+  createMessageQuery: `INSERT INTO messages
+    (name,method,contact,title,body,"from",day,time)
+    VALUES($1,$2,$3,$4,$5,$6,$7,$8);`,
   deleteMessagesTableQuery: `DROP TABLE messages;`,
 };

69769997ba313985c6b040cf05172256a01f2f2d
ngoakor12 <ngoakor12@gmail.com>
2022-11-13 15:07:46 +0200

    chore: able to view messages endpoint working

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const app = require("express")();
+const { viewMessage, viewMessages } = require("../db/functions");
 const cors = require("cors");
+const { pool } = require("../db/config");
 
 const PORT = 3001;
 const API_BASE_URL = `http://localhost:${PORT}`;
@@ -13,7 +15,28 @@ app.use(
 app.get("/", (_, res) => {
   res.status(200).json({ message: "server working flawlessly" });
 });
-
-app.listen(process.env.PORT || PORT, () => {
-  console.log(`App running at ${API_BASE_URL}`);
+app.get("/messages", async (_, res) => {
+  try {
+    const result = await viewMessages();
+    res.status(200).json({ success: true, data: result.rows });
+  } catch (error) {
+    console.error(error);
+    res.json(error);
+  }
 });
+
+// connect to db and start server
+pool
+  .connect()
+  .then(async () => {
+    console.log("Successfully connected to db...");
+    app.listen(process.env.PORT || PORT, () => {
+      console.log(`App running at ${API_BASE_URL}`);
+    });
+  })
+  .catch((error) => {
+    throw new Error(
+      "A problem occurred while connecting to the db. Try again later.",
+      error
+    );
+  });

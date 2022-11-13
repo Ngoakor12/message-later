5f50dcce3bfd9dac6477b29c5a5e6f11470ddcae
ngoakor12 <ngoakor12@gmail.com>
2022-11-13 14:20:55 +0200

    chore: able to edit a message

--- backend/db/functions.js
+++ backend/db/functions.js
@@ -7,6 +7,7 @@ const {
   deleteMessageQuery,
   deleteMessagesQuery,
   viewMessageQuery,
+  updateMessageQuery,
 } = require("./queries");
 const validateResultWithId = require("./utils");
 
@@ -57,6 +58,32 @@ async function viewMessage(id) {
   return result;
 }
 
+async function updateMessage(
+  id,
+  name,
+  method,
+  contact,
+  title,
+  body,
+  from,
+  day,
+  time
+) {
+  const result = await pool.query(updateMessageQuery, [
+    id,
+    name,
+    method,
+    contact,
+    title,
+    body,
+    from,
+    day,
+    time,
+  ]);
+  validateResultWithId(result);
+  return result;
+}
+
 // createMessagesTable().then((res) => console.log(res));
 
 // deleteMessagesTable().then((res) => console.log(res));
@@ -77,3 +104,15 @@ async function viewMessage(id) {
 // deleteMessages(0).then((res) => console.log(res));
 
 // viewMessage(0).then((res) => console.log(res));
+
+// updateMessage(
+//   2,
+//   "de bruyne",
+//   "email",
+//   "debruyne@mancity.com",
+//   "question",
+//   "are you a robot?",
+//   "not police",
+//   "12-12-2022",
+//   "12:00"
+// ).then((res) => console.log(res));
--- backend/db/queries.js
+++ backend/db/queries.js
@@ -13,6 +13,16 @@ module.exports = {
   createMessageQuery: `INSERT INTO messages
     (name,method,contact,title,body,"from",day,time)
     VALUES($1,$2,$3,$4,$5,$6,$7,$8);`,
+  updateMessageQuery: `UPDATE messages
+    SET name = $2,
+      method = $3,
+      contact = $4,
+      title = $5,
+      body = $6,
+      "from" = $7,
+      day = $8,
+      time = $9
+    WHERE id = $1;`,
   deleteMessageQuery: `DELETE FROM messages WHERE id=$1;`,
   deleteMessagesQuery: `DELETE FROM messages;`,
   viewMessageQuery: `SELECT * FROM messages WHERE id=$1;`,

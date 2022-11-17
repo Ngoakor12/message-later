2cbcac9647ffbe8ba654e22e2c64a4e4f3a2bb2c
ngoakor12 <ngoakor12@gmail.com>
2022-11-17 23:04:21 +0200

    fix validateId function bug

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,5 +1,6 @@
 function validateId(id) {
-  if (!typeof Number(id) !== "number") {
+  const convertedIdNumber = Number(id);
+  if (typeof convertedIdNumber !== "number") {
     throw new Error("'id' should be a number. Please try again.");
   }
 }

3671568cea0a9f1dd988ec3cce14f1891482b326
ngoakor12 <ngoakor12@gmail.com>
2022-11-20 10:41:04 +0200

    create date helper functions

--- backend/db/utils.js
+++ backend/db/utils.js
@@ -4,4 +4,32 @@ function validateResultWithId(resObject) {
   }
 }
 
+function createUCTDate(date, time) {
+  const [month, day, year] = date.split("-");
+  const [hours, seconds] = time.split(":");
+  const newDate = new Date(year, month - 1, day, hours, seconds);
+  return newDate;
+}
+
+function convertUTCDateToLocalDate(date) {
+  const newDate = new Date(date);
+  newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  return newDate;
+}
+
+function convertDateToISOString(date) {
+  const newDate = date.toISOString();
+  return newDate;
+}
+
 module.exports = validateResultWithId;
+
+// const uctD = createUCTDate("12-12-2022", "13:00");
+// const localD = convertUTCDateToLocalDate(uctD);
+// const uctDString = convertDateToISOString(uctD);
+// const localDString = convertDateToISOString(localD);
+
+// console.log("uct", uctD);
+// console.log("local", localD);
+// console.log("uct string", uctDString);
+// console.log("local string", localDString);

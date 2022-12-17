c59f68074e68182950c68af3e0015274b9605de5
ngoakor12 <ngoakor12@gmail.com>
2022-12-17 20:30:08 +0200

    remove unused utils

--- backend/database/utils.js
+++ backend/database/utils.js
@@ -21,42 +21,9 @@ function validateArguments(...args) {
   });
 }
 
-// function createUCTDate(date, time) {
-//   const [month, day, year] = date.split("-");
-//   const [hours, seconds] = time.split(":");
-//   const newDate = new Date(year, month - 1, day, hours, seconds);
-//   return newDate;
-// }
-
-// function convertUTCDateToLocalDate(date) {
-//   const newDate = new Date(date);
-//   newDate.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-//   return newDate;
-// }
-
-// function convertDateToISOString(date) {
-//   const newDate = date.toISOString();
-//   return newDate;
-// }
-
-// function convertISOStringToDate(date) {
-//   const newDate = new Date(date);
-//   return newDate;
-// }
-
-// function getCurrentUCTDate() {
-//   const date = new Date();
-//   return date;
-// }
-
 module.exports = {
   validateResultWithId,
   validateArguments,
-  // createUCTDate,
-  // convertUTCDateToLocalDate,
-  // convertDateToISOString,
-  // convertISOStringToDate,
-  // getCurrentUCTDate,
 };
 
 // const uctD = createUCTDate("12-12-2022", "13:00");

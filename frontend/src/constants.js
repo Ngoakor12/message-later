7088c751f4662b2a5f85b425739e9b12df08b5f6
ngoakor12 <ngoakor12@gmail.com>
2022-12-18 09:34:03 +0200

    fix empty constant string

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -4,6 +4,5 @@ export const DELETE_MESSAGE_ERROR =
   "Something went wrong. Message not deleted. Please try again.";
 export const UPDATE_MESSAGE_ERROR =
   "Something went wrong. Message not updated. Please try again.";
-export const CANCEL_EDIT_MESSAGE_CONFIRM = "";
-
+export const CANCEL_EDIT_MESSAGE_CONFIRM = "Cancel editing message?";
 export const DISCARD_CREATE_MESSAGE_CONFIRM = "Discard message?";

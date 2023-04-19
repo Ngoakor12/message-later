ecf0d0165b8e962ff5545d867de04a33a1db0063
ngoakor12 <ngoakor12@gmail.com>
2023-04-19 06:33:23 +0200

    Some more spacing

--- frontend/src/features/messages/AllMessages.jsx
+++ frontend/src/features/messages/AllMessages.jsx
@@ -1,8 +1,16 @@
+import { cleanDate, todayLocaleDateString } from "@ngoakor12/date-time-utils";
+
 function AllMessages() {
+  const date = todayLocaleDateString();
+  const cleanedDate = cleanDate(date);
   return (
-    <>
-      <h2>AllMessages</h2>
-    </>
+    <main className="all-messages-main">
+      <div className="today-date-container">
+        <h1>All messages</h1>
+        <div className="horizontal-line"></div>
+        <div className="today-date">{cleanedDate}</div>
+      </div>
+    </main>
   );
 }
 
--- frontend/src/features/messages/Drafts.jsx
+++ frontend/src/features/messages/Drafts.jsx
@@ -1,8 +1,16 @@
+import { cleanDate, todayLocaleDateString } from "@ngoakor12/date-time-utils";
+
 function Drafts() {
+  const date = todayLocaleDateString();
+  const cleanedDate = cleanDate(date);
   return (
-    <>
-      <h2>Drafts</h2>
-    </>
+    <main className="drafts-main">
+      <div className="today-date-container">
+        <h1>Drafts</h1>
+        <div className="horizontal-line"></div>
+        <div className="today-date">{cleanedDate}</div>
+      </div>
+    </main>
   );
 }
 

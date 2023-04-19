32d0bd5bab58e53335776789de90e0110eaf6eef
ngoakor12 <ngoakor12@gmail.com>
2023-04-19 06:48:12 +0200

    added logout button

--- frontend/src/features/navigation/Navigation.jsx
+++ frontend/src/features/navigation/Navigation.jsx
@@ -1,6 +1,13 @@
 import { NavLink, useLocation } from "react-router-dom";
+
+function logout() {}
+
 function Navigation() {
   const { pathname } = useLocation();
+
+  function handleClickLogout() {
+    console.log("Logging out");
+  }
 
   return (
     <nav
@@ -8,30 +15,37 @@ function Navigation() {
         pathname === `/` || pathname === `/login` ? "hide-component" : ""
       }`}
     >
-      <NavLink
-        to={"/today"}
-        className={({ isActive }) =>
-          isActive ? "nav-item active-nav-item" : "nav-item"
-        }
-      >
-        Today
-      </NavLink>
-      <NavLink
-        to={"/all-messages"}
-        className={({ isActive }) =>
-          isActive ? "nav-item active-nav-item" : "nav-item"
-        }
-      >
-        All Messages
-      </NavLink>
-      <NavLink
-        to={"/drafts"}
-        className={({ isActive }) =>
-          isActive ? "nav-item active-nav-item" : "nav-item"
-        }
-      >
-        Drafts
-      </NavLink>
+      <div className="nav-left">
+        <NavLink
+          to={"/today"}
+          className={({ isActive }) =>
+            isActive ? "nav-item active-nav-item" : "nav-item"
+          }
+        >
+          Today
+        </NavLink>
+        <NavLink
+          to={"/all-messages"}
+          className={({ isActive }) =>
+            isActive ? "nav-item active-nav-item" : "nav-item"
+          }
+        >
+          All Messages
+        </NavLink>
+        <NavLink
+          to={"/drafts"}
+          className={({ isActive }) =>
+            isActive ? "nav-item active-nav-item" : "nav-item"
+          }
+        >
+          Drafts
+        </NavLink>
+      </div>
+      <div className="nav-right">
+        <button onClick={handleClickLogout} className="logout-button">
+          Logout
+        </button>
+      </div>
     </nav>
   );
 }
